8bb88224945a34feb2318664a0cb84d9393c1575
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-20 19:31:42 +0530

    Enter first Name

---
+++ UserRegistrationForm/UserRegistrationForm/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UserRegistrationForm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============= WELCOME TO USER REGISTRTION FORM ===================");
+
+            UserForm.userForm();
+        }
+    }
+}
---
+++ UserRegistrationForm/UserRegistrationForm/UserForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationForm
+{
+    public class UserForm
+    {
+        public static void userForm()
+        {
+            string first = @"^[A-Z]{1}[a-z A-Z]{2,10}";
+            Console.WriteLine("Enter first name : ");
+            string fName = Console.ReadLine();
+            Regex name = new Regex(first);
+            bool n = name.IsMatch(fName);
+            if (n)
+            {
+                Console.WriteLine($" Valide First Name {fName}");
+            }
+            else
+            {
+                Console.WriteLine("Invalide first name");
+            }
+        }
+    }
+}
