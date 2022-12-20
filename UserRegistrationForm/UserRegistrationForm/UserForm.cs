8c1c4909e0e76f98db0ca63e7b3142f65a56ef4d
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-20 19:33:56 +0530

    Enter last name

--- UserRegistrationForm/UserRegistrationForm/UserForm.cs
+++ UserRegistrationForm/UserRegistrationForm/UserForm.cs
@@ -12,17 +12,31 @@ namespace UserRegistrationForm
         public static void userForm()
         {
             string first = @"^[A-Z]{1}[a-z A-Z]{2,10}";
-            Console.WriteLine("Enter first name : ");
+            Console.Write("Enter first name : ");
             string fName = Console.ReadLine();
+            Console.Write("Enter last name : ");
+            string lName = Console.ReadLine();
+
             Regex name = new Regex(first);
-            bool n = name.IsMatch(fName);
-            if (n)
+
+            bool frst = name.IsMatch(fName);
+            bool lst = name.IsMatch(lName);
+
+            if (frst)
             {
                 Console.WriteLine($" Valide First Name {fName}");
             }
             else
             {
                 Console.WriteLine("Invalide first name");
+            }
+            if (lst)
+            {
+                Console.WriteLine($" Valide Last Name {lName}");
+            }
+            else
+            {
+                Console.WriteLine("Invalide last name");
             }
         }
     }
